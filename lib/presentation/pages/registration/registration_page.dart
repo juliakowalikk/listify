2f9324e6d8881c82685c8fc4cd8aec54b261de6c
juliakowalikk <kowalikjulia00@gmail.com>
2023-10-29 22:17:26 +0100

    Code refractor

--- lib/presentation/pages/registration/registration_page.dart
+++ lib/presentation/pages/registration/registration_page.dart
@@ -1,9 +1,8 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:listify/presentation/pages/list/list_page.dart';
 import 'package:listify/presentation/pages/registration/cubit/registration_cubit.dart';
-import 'package:listify/presentation/pages/sign_in/sign_in_page.dart';
+import 'package:listify/presentation/pages/registration/widgets/register_sign_in_now.dart';
 import 'package:listify/presentation/widgets/listify_text_field.dart';
 
 class Registration extends StatefulWidget {
@@ -31,100 +30,77 @@ class _RegistrationState extends State<Registration> {
   Widget build(BuildContext context) => BlocProvider(
         create: (context) => RegistrationCubit(),
         child: Builder(
-            builder: (context) => ScaffoldMessenger(
-                  child: BlocListener<RegistrationCubit, RegistrationState>(
-                    listener: _listener,
-                    child: Scaffold(
-                      body: SafeArea(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Form(
-                            key: _formKey,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Text(
-                                  'Welcome to Listify!',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w800,
-                                      fontSize: 20),
-                                ),
-                                const Text('Register to create your account'),
-                                UserTextField(
-                                  controller: nameController,
-                                  hintText: 'Name',
-                                  isTextVisible: false,
-                                ),
-                                UserTextField(
-                                  validator: (value) {
-                                    if (value == null || value.isEmpty) {
-                                      return 'Please enter email';
-                                    }
-                                    return null;
-                                  },
-                                  controller: emailController,
-                                  hintText: 'Email address',
-                                  isTextVisible: false,
-                                ),
-                                UserTextField(
-                                  validator: (value) {
-                                    if (value == null || value.isEmpty) {
-                                      return 'Please enter password';
-                                    } else if (value.length < 8) {
-                                      return 'Password must be at least 8 letters long';
-                                    }
-                                    return null;
-                                  },
-                                  controller: passwordController,
-                                  hintText: 'Password',
-                                  isTextVisible: true,
-                                ),
-                                ElevatedButton(
-                                  style: ElevatedButton.styleFrom(
-                                      backgroundColor: Colors.grey.shade800),
-                                  onPressed: () {
-                                    if (_formKey.currentState!.validate()) {
-                                      context
-                                          .read<RegistrationCubit>()
-                                          .register(emailController.text.trim(),
-                                              passwordController.text.trim());
-                                    }
-                                  },
-                                  child: const Text(
-                                    'Sign Up',
-                                  ),
-                                ),
-                                RichText(
-                                  text: TextSpan(
-                                    children: <TextSpan>[
-                                      const TextSpan(
-                                          text: 'Are you already a user?',
-                                          style:
-                                              TextStyle(color: Colors.black)),
-                                      TextSpan(
-                                        text: ' Log in now!',
-                                        style:
-                                            const TextStyle(color: Colors.blue),
-                                        recognizer: TapGestureRecognizer()
-                                          ..onTap = () => Navigator.push(
-                                                context,
-                                                MaterialPageRoute(
-                                                  builder: (context) =>
-                                                      const SignIn(),
-                                                ),
-                                              ),
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ],
+          builder: (context) => ScaffoldMessenger(
+            child: BlocListener<RegistrationCubit, RegistrationState>(
+              listener: _listener,
+              child: Scaffold(
+                body: SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Welcome to Listify!',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w800, fontSize: 20),
+                          ),
+                          const Text('Register to create your account'),
+                          UserTextField(
+                            controller: nameController,
+                            hintText: 'Name',
+                            isTextVisible: false,
+                          ),
+                          UserTextField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter email';
+                              }
+                              return null;
+                            },
+                            controller: emailController,
+                            hintText: 'Email address',
+                            isTextVisible: false,
+                          ),
+                          UserTextField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter password';
+                              } else if (value.length < 8) {
+                                return 'Password must be at least 8 letters long';
+                              }
+                              return null;
+                            },
+                            controller: passwordController,
+                            hintText: 'Password',
+                            isTextVisible: true,
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.grey.shade800),
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                context.read<RegistrationCubit>().register(
+                                    emailController.text.trim(),
+                                    passwordController.text.trim());
+                              }
+                            },
+                            child: const Text(
+                              'Sign Up',
                             ),
                           ),
-                        ),
+                          const RegisterSignInNow(),
+                        ],
                       ),
                     ),
                   ),
-                )),
+                ),
+              ),
+            ),
+          ),
+        ),
       );
 
   void _listener(BuildContext context, RegistrationState state) =>
---
+++ lib/presentation/pages/registration/widgets/register_sign_in_now.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:listify/presentation/pages/sign_in/sign_in_page.dart';
+
+class RegisterSignInNow extends StatelessWidget {
+  const RegisterSignInNow({super.key});
+
+  @override
+  Widget build(BuildContext context) => RichText(
+        text: TextSpan(
+          children: <TextSpan>[
+            const TextSpan(
+                text: 'Are you already a user?',
+                style: TextStyle(color: Colors.black)),
+            TextSpan(
+              text: ' Log in now!',
+              style: const TextStyle(color: Colors.blue),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () => Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SignIn(),
+                      ),
+                    ),
+            ),
+          ],
+        ),
+      );
+}
